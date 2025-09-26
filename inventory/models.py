6901590b05cf95c7e13bcbc3864f6e9bb5ccaf35
Juan Diego Quintero Álvarez <tu_correo@ejemplo.com>
2025-09-26 13:15:38 +0000

    Descripción de mis cambios

--- inventory/models.py
+++ inventory/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class InventoryProduct(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=200)  # 👈 nuevo campo
+    name = models.CharField(max_length=200)  
     barcode = models.CharField(unique=True, max_length=64)
     location = models.CharField(max_length=255)
     quantity = models.IntegerField()
-    last_updated = models.DateTimeField(auto_now=True)  # 👈 se actualiza automáticamente
+    last_updated = models.DateTimeField(auto_now=True) 
 
     class Meta:
-        db_table = 'inventory_product'  # mantenemos la misma tabla
+        db_table = 'inventory_product' 
 
